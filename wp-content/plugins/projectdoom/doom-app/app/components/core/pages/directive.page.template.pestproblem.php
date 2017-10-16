5d4ece829f634a85357c844c0df0f09364bd39e2
Max Sibande <Max.Sibande@tbwa.co.za>
2017-10-16 23:40:34 +0200

    Added Pest Problem.php & contactform js

---
+++ wp-content/plugins/projectdoom/doom-app/app/components/core/pages/directive.page.template.pestproblem.php
@@ -0,0 +1,101 @@
+                        <div flex="100" flex-gt-md="50" class="home-copy-content" flex-order="2" flex-order-gt-md="1"  layout="column" style="text-align:center;">
+							
+							<div class="" layout="column" style="text-align:center;">
+								
+								<h1 class="page-title" style="margin-top:50px;text-align:left;font-size:35pt;">What's Bugging you?</h1> 
+								<span class="hr-divider" style=""></span>
+								<p class="page-description" style="margin-bottom:30px;text-align:left;">{{pageContent.post_content}}</p>
+								
+							</div>
+							<?php /**/ ?>
+							<div class="insect-filter-categories center-content mobile-padding" style="" layout-gt-md="row" layout-align-gt-md="center center" >
+								<md-button
+									class="md-button primary-tag"
+									style=""
+									data-ng-click="filterInsects('flying')"
+									data-ng-class="{active: pageContent.filter_type == 'flying'}">
+									Flying
+								</md-button>
+								<md-button
+									class="md-button primary-tag"
+									style=""
+									data-ng-click="filterInsects('crawling')"
+									data-ng-class="{active: pageContent.filter_type == 'crawling'}">
+									Crawling
+								</md-button>
+								<md-button
+									class="md-button primary-tag "
+									data-ng-click="filterInsects('all')"
+									style=""
+									data-ng-class="{active: pageContent.filter_type == 'all'}">
+									All
+								</md-button>
+							</div> 
+							<?php /**/?>
+							<div class="center-content" style="width:100%;"  layout-gt-md="row" layout-align-gt-md="center center" style="margin:40px auto;text-align:center;">
+								<ul class="product-insects home-activity" insect-type="all"> 
+									<li data-ng-class="{active: pageContent.insect_ant == true, stat: activeInsect.post_name == 'ant' }" class="icon-insect-ant" title="Ants">
+										<button type="button" data-ng-disabled="!pageContent.insect_ant" class="md-btn" data-ng-click="whatsBuggin('ant')">
+											<i class="icon-ant"></i>
+											<br />
+											<p>Ants</p>
+										</button>
+									</li>
+									<li data-ng-class="{active: pageContent.insect_cockroach == true, stat: activeInsect.post_name == 'cockroach'}" class="icon-insect-cockroach" title="Cockroaches">
+										<button type="button" data-ng-disabled="!pageContent.insect_cockroach" class="md-btn" data-ng-click="whatsBuggin('cockroach')">
+											<i class="icon-cockroach"></i>
+											<br />
+											<p>Cockroaches</p>
+										</button>
+									</li>
+									<li data-ng-class="{active: pageContent.insect_fishmoth == true, stat: activeInsect.post_name == 'fishmoth'}" class="icon-insect-fishmoth" title="Fishmoths">
+										<button type="button" data-ng-disabled="!pageContent.insect_fishmoth" class="md-btn" data-ng-click="whatsBuggin('fishmoth')">
+											<i class="icon-fishmoth"></i>
+											<br />
+											<p>fishmoths</p>
+										</button>
+									</li>
+									<li data-ng-class="{active: pageContent.insect_flea == true, stat: activeInsect.post_name == 'flea'}" class="icon-insect-flea" title="Fleas">
+										<button type="button" data-ng-disabled="!pageContent.insect_flea" class="md-btn" data-ng-click="whatsBuggin('flea')">
+											<i class="icon-flea"></i>
+											<br />
+											<p>fleas</p>
+										</button>
+									</li>
+									<li data-ng-class="{active: pageContent.insect_fly == true, stat: activeInsect.post_name == 'fly'}" class="icon-insect-fly" title="Flies">
+										<button type="button" data-ng-disabled="!pageContent.insect_fly" class="md-btn" data-ng-click="whatsBuggin('fly')">	
+											<i class="icon-fly"></i>
+											<br />
+											<p>flies</p>
+										</button>
+									</li>
+									<li data-ng-class="{active: pageContent.insect_mosquito == true, stat: activeInsect.post_name == 'mosquito'}" class="icon-insect-mosquito" title="Mosquitoes">
+										<button  data-ng-disabled="!pageContent.insect_mosquito" type="button" class="md-btn"  data-ng-click="whatsBuggin('mosquito')">
+											<i class="icon-mosquito"></i>
+											<br />
+											<p>Mosquitoes</p>
+										</button>
+									</li>
+								</ul>
+							</div>
+							<div class="" layout="column">
+								
+								<div layout-gt-md="row" layout-fill layout-align="center center" style="margin:50px auto;margin-bottom:0;">
+
+                                    <ui-randomstat id="home-page-stat" ishome="true" active-insect="{{activeInsect.post_name}}" duration="3000"></ui-randomstat>
+                                    <!--<ui-randomstat  layout="column" flex="100" timer="true"></ui-randomstat>-->
+
+                                </div>
+								
+
+                            </div>
+                            <div class="" layout="column" style="" layout-align="center center">
+								
+                                <md-button ng-click="goto(activeInsect.post_name)" class="md-primary primary-cta" style="padding:10px 45px;font-size:30px;margin: 44px 0;width:300px;">start</md-button>
+
+							</div>
+
+						</div>
+						<div flex="100" flex-offset-gt-md="10" flex-gt-md="40" flex-order="1" flex-order-gt-md="2" class="" style="" layout-align="center center">
+							<img data-ng-src="{{activeInsect.image}}" data-ng-if="activeInsect.image" data-ng-click="" flex="100" class="super-bug-image" alt="" style="position:relative;left:0;top:0;" />
+						</div>
---
+++ wp-content/plugins/projectdoom/doom-app/app/components/shared/configurator/contactform/contactform.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Display Contact Content
+ *
+ * @package WordPress
+ * @subpackage Doom
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+	<div layout="column" layout-align="center center" flex="100"> 
+		<div layout="row" layout-fill layout-md="column" >
+
+			<div class="md-padding" flex-md="100" flex="60" flex-order-xs="2" style="background-color:#ffffff;">
+
+                <form id="formContact" name="userForm" novalidate>
+
+                    <div layout-gt-sm="row">
+                        <md-input-container class="md-block" flex-gt-sm>
+                            <label for="name">Your Name</label>
+                            <input
+                                name="name"
+                                required
+                                md-maxlength="30"
+                                data-ng-model="formInputs.firstName">
+                            <div ng-messages="formContact.firstName.$error">
+                                <div ng-message="required">Please provide us with a first name.</div>
+                                <!--<div ng-message="md-maxlength">Your first name must be less than 30 characters long.</div>-->
+                            </div>
+                        </md-input-container>
+
+                        <md-input-container class="md-block" flex-gt-sm>
+                            <label for="email">Email Address</label>
+                            <input 
+                                type="email"
+                                name="email"
+                                required
+                                
+                                data-ng-model="formInputs.emailAddress"
+                                minlength="10"  data-ng-pattern="/^.+@.+\..+$/" >
+                            <div ng-messages="formContact.emailAddress.$error">
+                                <div ng-message="required">Please provide your an email address.</div>
+                                <!--<div ng-message="md-email">Please provide us with a valid email address.</div>-->
+                            </div>
+                        </md-input-container>
+                    </div>
+
+                    <md-input-container md-no-float class="md-block"></md-input-container>
+
+                    <div layout-gt-xs="row">
+                        <md-input-container class="md-block" flex-gt-xs>
+                            <label for="phone">Phone</label>
+                            <input 
+                                name="phone"
+                                type="tel"
+                                required
+                                data-ng-patterm="^(\+27|27)?(\()?0?[87][23467](\))?( |-|\.|_)?(\d{3})( |-|\.|_)?(\d{4})"
+                                data-ng-model="formInputs.phone"
+                                minlength="10" maxlength="100">
+                            <div ng-messages="formContact.phone.$error">
+                                <div ng-message="required">Please provide your contact number.</div>
+                            </div>
+                        </md-input-container>
+
+                        <md-input-container class="md-block" flex-gt-xs>
+                            <label for="company">Company</label>
+                            <input 
+                                name="company"
+                                data-ng-model="formInputs.company">
+                            <div ng-messages="formContact.company.$error">
+                                <div ng-message="md-maxlength">Your company name must be less than 100 characters long.</div>
+                            </div>
+                        </md-input-container>
+                    </div>
+
+                    <md-input-container md-no-float class="md-block"></md-input-container>
+
+                    <md-input-container class="md-block">
+                        <label for="message">Message</label>
+                        <textarea 
+                            name="message"
+                            required
+                            maxlength="255"
+                            data-ng-model="formInputs.message" md-maxlength="255" 
+                            rows="5" md-select-on-focus>
+                        </textarea>
+                    </md-input-container>
+
+                    <md-input-container md-no-float class="md-block"></md-input-container>
+
+                    <md-input-container class="md-block" layout-align="end center">
+                        <!--<button type="button" class="md-button" data-ng-submit="save()">Send</button>-->
+                        <md-button type="button" data-ng-click="submitForm()" class="primary-cta">Send</md-button>
+                    </md-input-container>
+
+                </form>
+
+                <div data-ng-transclude></div>
+
+            </div>
+            <div class="" flex-md="100" flex="40" flex-order-xs="1" style="background-color:#7B7B7A;color:#ffffff;">
+                <div data-ui-google-map
+                    data-map
+					zoom="10" 
+					center="[-26.0752982, 28.0242038]"
+					scrollwheel = "false"
+					disable-default-u-i="false"
+					disable-double-click-zoom="false"
+					draggable="true"
+					draggable-cursor="help"
+					dragging-cursor="move"
+					navigationControl ="true"
+					keyboard-shortcuts="false"
+					max-zoom="20"
+					min-zoom="6"
+                    tilt="45"
+                    flex-order-xs="2"
+                    style="color:#444;text-align:center;padding:0;"
+                    map-type-id="ROADMAP"></div>
+                <div class="md-padding" flex-order-xs="1">
+                    <h3 style="margin-top:5px;">CONTACT INFORMATION</h3>
+
+                    <p><i class="lnr lnr-map-marker"></i>&nbsp;&nbsp;Tiger Brands Limited,<br />3010 William Nicol Drive, Bryanston<br />2021.</p>
+
+                    <p><i class="lnr lnr-phone"></i>&nbsp;&nbsp;0860 101 107</p>
+
+                    <p><i class="lnr lnr-inbox"></i>&nbsp;&nbsp;<a href="mailto:tigercsd@tigerbrands.com" style="text-decoration:none;color:#fff;">tigercsd@tigerbrands.com</a></p> 
+                    <br /><br />
+                </div>
+                
+            </div>
+
+        </div>   
+	</div>
